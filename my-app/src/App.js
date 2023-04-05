9365c43e916c7c4aa93c28f9a5b2a53c5420ffb1
JamieBirch <jamie.d.birch@gmail.com>
2023-04-05 19:10:13 +0200

    Add AG Grid + react

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import "./App.css";
+import MyGrid from "./MyGrid";
+
+function App() {
+  return (
+    <div className="App">
+      <MyGrid />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/MyGrid.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+const MyGrid = () => {
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+  };
+
+  const columnDefs = [
+    { headerName: "Make", field: "make" },
+    { headerName: "Model", field: "model" },
+    { headerName: "Price", field: "price" },
+  ];
+
+  const rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 },
+  ];
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: "500px", width: "600px" }}>
+      <AgGridReact
+        onGridReady={onGridReady}
+        columnDefs={columnDefs}
+        rowData={rowData}
+      />
+    </div>
+  );
+};
+
+export default MyGrid;
