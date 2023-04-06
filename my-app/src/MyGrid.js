a0a1e79f304adbf66b8281d4006e390656046221
JamieBirch <jamie.d.birch@gmail.com>
2023-04-06 19:47:47 +0200

    Refactor attempt

--- my-app/src/MyGrid.js
+++ my-app/src/MyGrid.js
@@ -2,19 +2,30 @@ import React, { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { getIngredients2 } from "./api/IngredientsAPI.js";
 
 const MyGrid = () => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
   const [rowData, setRowData] = useState([]);
 
-  useEffect(() => {
+/*  useEffect(() => {
     fetch("/ingredient")
       .then((response) => response.json())
       .then((data) => {
         setRowData(data);
       });
+  }, []);*/
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      const ingredients = await getIngredients2();
+      setRowData(ingredients);
+    };
+
+    fetchIngredients();
   }, []);
+
 
   const onGridReady = (params) => {
     setGridApi(params.api);
---
+++ my-app/src/api/IngredientsAPI.js
@@ -0,0 +1,46 @@
+// Ingredients
+
+const BASE_URL = "http://localhost:8080/ingredient";
+
+export const getIngredients2 = async () => {
+  const response = await fetch(`${BASE_URL}`);
+  return handleResponse(response);
+};
+
+/*export function getIngredients() {
+    var url = "/ingredient";
+    var foodplanPromise = $http.get(url);
+    return foodplanPromise.then(function(response) {
+        return response.data;
+    });
+}*/
+
+/*export function deleteIngredient(id) {
+    var url = "/ingredient/" + id;
+    var foodplanPromise = $http.delete(url);
+    return foodplanPromise.then(function(response) {
+        getIngredients();
+    });
+}
+
+export function addIngredient(name) {
+    var url = "/ingredient";
+    var myJson = { "name": name };
+    return $http.post(url, myJson)
+        .then(function(response) {
+            console.log('Ingredient added:', response.data);
+            getIngredients();
+        })
+        .catch(function(error) {
+            console.error('There was a problem adding the ingredient:', error);
+        });
+}*/
+
+// function to handle response from API
+const handleResponse = async (response) => {
+  if (!response.ok) {
+    const error = await response.text();
+    throw new Error(error);
+  }
+  return response.json();
+};
